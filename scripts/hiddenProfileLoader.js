aec481a0f34afdc1b0e87e47066a2b89b7c889c5
yanicknedderhoff <yanick@brandwatch.com>
2018-05-07 18:25:03 +0200

    Add hidden profile, to be filled later

---
+++ scripts/hiddenProfileLoader.js
@@ -0,0 +1,8 @@
+function toggleHiddenProfile() {
+    var x = document.getElementById("hiddenProfile");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
