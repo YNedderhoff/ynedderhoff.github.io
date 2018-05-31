48a15a487bd7cc9cd9c33fd970b1fcacbe42cd0b
yanicknedderhoff <yanick@brandwatch.com>
2018-06-01 01:05:50 +0200

    move output comment in color_sorter.py

--- scripts/python/color_sorter.py
+++ scripts/python/color_sorter.py
@@ -8,6 +8,6 @@ def get_hsv(hexrgb):
     return colorsys.rgb_to_hsv(r, g, b)
 
 color_list = ["#8BC35A", "#00ACEE", "#000000", "#0077b5", "#FD6F31", "#FC561F", "#23364B", "#DBD2BF", "#FC4820", "#0077b5", "#F27F33", "#D7D0BF", "#1CD156"]
-# output: ['#000000', '#FC4820', '#FC561F', '#FD6F31', '#F27F33', '#DBD2BF', '#D7D0BF', '#8BC35A', '#1CD156', '#00ACEE', '#0077b5', '#0077b5', '#23364B']
 color_list.sort(key=get_hsv)
 print(color_list)
+# output: ['#000000', '#FC4820', '#FC561F', '#FD6F31', '#F27F33', '#DBD2BF', '#D7D0BF', '#8BC35A', '#1CD156', '#00ACEE', '#0077b5', '#0077b5', '#23364B']
