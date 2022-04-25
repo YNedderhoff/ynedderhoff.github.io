1cf3640e80c0c20573e780480fbe30b4e808afcd
ynedderhoff <hey@yanick.xyz>
2022-04-26 00:45:54 +0200

    make color sorting script python 3 ready and pep compliant

--- scripts/python/color_sorter.py
+++ scripts/python/color_sorter.py
@@ -1,10 +1,11 @@
-### https://stackoverflow.com/questions/8915113/sort-hex-colors-to-match-rainbow
+# https://stackoverflow.com/questions/8915113/sort-hex-colors-to-match-rainbow
 
 import colorsys
 
+
 def get_hsv(hexrgb):
-    hexrgb = hexrgb.lstrip("#")   # in case you have Web color specs
-    r, g, b = (int(hexrgb[i:i+2], 16) / 255.0 for i in xrange(0,5,2))
+    hexrgb = hexrgb.lstrip("#")  # in case you have Web color specs
+    r, g, b = (int(hexrgb[i:i + 2], 16) / 255.0 for i in range(0, 5, 2))
     return colorsys.rgb_to_hsv(r, g, b)
 
 color_list = ["#8BC35A", "#00ACEE", "#000000", "#0077b5", "#FD6F31", "#FC561F", "#23364B", "#DBD2BF", "#FC4820", "#0077b5", "#F27F33", "#D7D0BF", "#1CD156", "#BA0200", "#3097E8", "#056467", "#E44734", "#08090A", "#FC6D25", "#19445D", "#FC5205"]
