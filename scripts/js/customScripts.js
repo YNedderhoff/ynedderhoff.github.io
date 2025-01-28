91695d62f6ea9138def741754bb748ee6b8a8e63
ynedderhoff <hey@yanick.xyz>
2025-01-29 00:42:28 +0100

    Introduce hidden social links expandable using a caret

--- scripts/js/customScripts.js
+++ scripts/js/customScripts.js
@@ -6,3 +6,20 @@ function toggleHiddenProfile() {
         x.style.display = "none";
     }
 }
+
+function toggleHiddenSocial() {
+  var hiddenLinks = document.getElementById("hiddenSocialLinks");
+  var button = document.getElementById("toggleSocialBtn");
+  var icon = button.querySelector("i");
+
+
+  if (hiddenLinks.style.display === "none") {
+    icon.classList.remove("fa-caret-down");  // Remove the caret down icon
+    icon.classList.add("fa-caret-up");       // Add the caret up icon (Show Less)
+    hiddenLinks.style.display = "block";
+  } else {
+    icon.classList.remove("fa-caret-up");    // Remove the caret up icon
+    icon.classList.add("fa-caret-down");     // Add the caret down icon (Show More)
+    hiddenLinks.style.display = "none";
+  }
+}
